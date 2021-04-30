c07d50332132fd68d728c307e9b8467a2e0f9d82
farid khan <70059547+faridkhan5@users.noreply.github.com>
2021-05-01 03:30:24 +0530

    Update p19.py

--- p19.py
+++ p19.py
@@ -3,24 +3,25 @@
 from time import time
 import datetime
 
-def sunday_count(s,e): #date = "dd:mm:yyyy"
+def sunday_count(s,e): #date format = "dd:mm:yyyy"
     day, month, yr = s.split(":")
     start_date = datetime.date(int(yr), int(month), int(day))
 
     day, month, yr = e.split(":")
     end_date = datetime.date(int(yr), int(month), int(day))
 
-    delta = datetime.timedelta(days=1)
+    delta = datetime.timedelta(days=1) #to iterate over the dates
 
-    day_name = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    day_name = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] #returns the name of day respective to the index returned by weekday()
     sunday_count = 0
     while start_date <= end_date:
-        day_index = start_date.weekday()
+        day_index = start_date.weekday() #weekday returns a index with monday -> [0] upto sunday -> [6]
         if start_date.day == 1 and day_name[day_index] == 'Sunday':
             sunday_count += 1
         start_date += delta
 
     return sunday_count
+
 
 sol = sunday_count('01:01:1901','31:12:2000')
 print('There are {} sundays in the start of every month between the given dates'.format(sol))
